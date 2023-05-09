71caeb6ad48c86d8058d8e46b0e7c3db6ef2c7cd
Breanna <b.gonzales21@yahoo.com>
2023-05-08 19:37:12 -0700

    added

--- easy.js
+++ easy.js
@@ -1,27 +1,28 @@
 // Find Method:
 // Challenge: Find the first even number in an array.
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Starter code - Add your solution here
-
+const firstEvenNumber = numbers.find(number => number % 2 ===0);
+console.log(firstEvenNumber)
 
 // Sort Method:
 // Challenge: Sort an array of numbers in ascending order.
 
-var unsortedArray = [5, 3, 8, 1, 9, 2, 7, 4, 6];
-
 // Starter code - Add your solution here
-
+var unsortedArray = [5, 3, 8, 1, 9, 2, 7, 4, 6];
+let sortedArray = unsortedArray.sort()
+console.log(sortedArray)
 
 // Filter Method:
 // Challenge: Filter out all the strings from an array of mixed data types.
 
-var mixedArray = [1, 'apple', 2, 'banana', 3, 'cherry', 4, 'date'];
+const mixedArray = [1, 'apple', 2, 'banana', 3, 'cherry', 4, 'date'];
 
 // Starter code - Add your solution here
-
-
+const filteredArray = mixedArray.filter(element => typeof element !== 'string');
+console.log(filteredArray)
 // Reduce Method:
 // Challenge: Calculate the sum of all numbers in an array.
 
